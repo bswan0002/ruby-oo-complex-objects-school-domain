d33833c5fda9849a013a90737a87474bfb7f4c84
bswan0002 <bswan0002@gmail.com>
2020-11-18 13:46:05 -0800

    tests pass

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] ? self.roster[grade] << name : self.roster[grade] = [name]
+  end
+
+  def grade(gradeLevel)
+    self.roster[gradeLevel]
+  end
+
+  def sort
+    self.roster.each_value { |studentArr| studentArr.sort!}
+  end
+end
